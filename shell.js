dad4d1712696a68d67e449043529f154a7f51c7a
zevan <1591787827@qq.com>
2022-04-01 09:39:53 +0800

    'deploy'

--- shell.js
+++ shell.js
@@ -4,6 +4,6 @@ if (shell.exec('npm run build').code !== 0) {//执行npm run build 命令
   shell.exit(1)
 }
 shell.exec('git init')
-shell.exec('git add .')
+shell.exec('git add -A')
 shell.exec("git commit -m 'deploy'")
 shell.exec('git push -f git@github.com:GuoFengZhang95/blog.git master:gh-pages')
