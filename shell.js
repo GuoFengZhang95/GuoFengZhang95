dd7256753e3b5761bdaa2c04bd054a34a700d0e1
zevan <1591787827@qq.com>
2022-04-01 09:31:29 +0800

    update:shell.js

--- shell.js
+++ shell.js
@@ -4,7 +4,7 @@ if (shell.exec('npm run build').code !== 0) {//执行npm run build 命令
   shell.echo('Error: Git commit failed')
   shell.exit(1)
 }
-shell.cd('docs/.vuepress/dist')
+shell.cd('docs/.vuepress/dist/')
 shell.exec('git add .')
 shell.exec("git commit -m 'deploy'")
 shell.exec('git push -f git@github.com:GuoFengZhang95/blog.git master:gh-pages')
