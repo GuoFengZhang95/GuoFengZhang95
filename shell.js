d0f16672498e4113c0b2c013ac4fe0b3753eea3e
zevan <1591787827@qq.com>
2022-04-01 09:40:36 +0800

    'deploy'

--- shell.js
+++ shell.js
@@ -1,9 +1,10 @@
 const shell = require('shelljs')
-if (shell.exec('npm run build').code !== 0) {//执行npm run build 命令
-  shell.echo('Error: Git commit failed')
-  shell.exit(1)
-}
-shell.exec('git init')
+// if (shell.exec('npm run build').code !== 0) {//执行npm run build 命令
+//   shell.echo('Error: Git commit failed')
+//   shell.exit(1)
+// }
+let res = shell.exec('git init')
+console.log(res)
 shell.exec('git add -A')
 shell.exec("git commit -m 'deploy'")
 shell.exec('git push -f git@github.com:GuoFengZhang95/blog.git master:gh-pages')
