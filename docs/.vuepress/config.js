577a30373addce014786ab8331edd0be60320865
LAPTOP-EA8Q439N\15917 <1591787827@qq.com>
2022-04-05 22:57:28 +0800

    add content

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,33 @@
 module.exports = {
-  title: "峰峰的笔记",
-  description: "javascript vue web node",
+  title: '峰峰的笔记',
+  description: 'javascript vue web node',
   base: '/blog/',
   themeConfig: {
     nav: [
-      { text: "指南", link: "/guide/one" },
-      { text: "Javascript", link: "/javascript/" },
-      { text: "Vue", link: "/vue/" },
-      { text: "Node", link: "/node/" },
-      { text: "其他", link: "/other/" },
-      { text: "External", link: "https://github.com/GuoFengZhang95" },
+      { text: '指南', link: '/guide/one' },
+      { text: 'Javascript', link: '/javascript/' },
+      { text: 'Vue', link: '/vue/' },
+      { text: 'Node', link: '/node/' },
+      { text: '其他', link: '/other/linux' },
+      { text: 'External', link: 'https://github.com/GuoFengZhang95' },
     ],
     displayAllHeaders: true, // 默认值：false
     /**配置侧边栏 auto/自定义 */
     sidebar: {
-      "/guide/": [
+      '/guide/': [
         {
-          title: "指南", // 必要的
+          title: '指南', // 必要的
           collapsable: true, // 可选的, 默认值是 true,
           sidebarDepth: 1, // 可选的, 默认值是 1
-          children: ["/guide/one", "/guide/two"],
+          children: ['/guide/one', '/guide/two'],
+        },
+      ],
+      '/other/': [
+        {
+          title: '其他',
+          collapsable: true,
+          sidebarDepth: 1,
+          children: ['/other/linux', '/other/ecs'],
         },
       ],
     },
---
+++ docs/other/linux.md
@@ -0,0 +1,4 @@
+## linux
+### 安装node.js
+在linux环境安装node.js
+
